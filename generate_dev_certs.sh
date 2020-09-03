96c4c1c90e9e44d67adfb8cde7b89be73b9ee717
Lars Kendall <lars@prominentedge.com>
2020-09-03 09:39:40 -0700

    Update generate dev certs flag for cross-compatibility.

--- generate_dev_certs.sh
+++ generate_dev_certs.sh
@@ -20,18 +20,18 @@ mkdir server/certs/user
 
 # Create Test Root Admin User Keys
 openssl genrsa -out server/certs/user/rootadmin.key 2048
-openssl req -new -key server/certs/user/rootadmin.key -subj "/C=US/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/OU=USER/CN=TEST.USER.ROOTADMIN.0000000001" --out server/certs/user/rootadmin.csr
+openssl req -new -key server/certs/user/rootadmin.key -subj "/C=US/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/OU=USER/CN=TEST.USER.ROOTADMIN.0000000001" -out server/certs/user/rootadmin.csr
 openssl x509 -req -in server/certs/user/rootadmin.csr -CA server/certs/ca.crt -CAkey server/certs/ca.key -CAcreateserial -out server/certs/user/rootadmin.crt
 openssl pkcs12 -export -out server/certs/user/rootadmin.p12 -inkey server/certs/user/rootadmin.key -in server/certs/user/rootadmin.crt -certfile server/certs/ca.crt -passout pass:
 
 # Create Test Org Admin User Keys
 openssl genrsa -out server/certs/user/orgadmin.key 2048
-openssl req -new -key server/certs/user/orgadmin.key -subj "/C=US/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/OU=USER/CN=TEST.USER.ORGADMIN.0000000002" --out server/certs/user/orgadmin.csr
+openssl req -new -key server/certs/user/orgadmin.key -subj "/C=US/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/OU=USER/CN=TEST.USER.ORGADMIN.0000000002" -out server/certs/user/orgadmin.csr
 openssl x509 -req -in server/certs/user/orgadmin.csr -CA server/certs/ca.crt -CAkey server/certs/ca.key -CAcreateserial -out server/certs/user/orgadmin.crt
 openssl pkcs12 -export -out server/certs/user/orgadmin.p12 -inkey server/certs/user/orgadmin.key -in server/certs/user/orgadmin.crt -certfile server/certs/ca.crt -passout pass:
 
 # Create Basic User Keys
 openssl genrsa -out server/certs/user/user.key 2048
-openssl req -new -key server/certs/user/user.key -subj "/C=US/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/OU=USER/CN=TEST.USER.BASIC.0000000003" --out server/certs/user/user.csr
+openssl req -new -key server/certs/user/user.key -subj "/C=US/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/OU=USER/CN=TEST.USER.BASIC.0000000003" -out server/certs/user/user.csr
 openssl x509 -req -in server/certs/user/user.csr -CA server/certs/ca.crt -CAkey server/certs/ca.key -CAcreateserial -out server/certs/user/user.crt
 openssl pkcs12 -export -out server/certs/user/user.p12 -inkey server/certs/user/user.key -in server/certs/user/user.crt -certfile server/certs/ca.crt -passout pass:
