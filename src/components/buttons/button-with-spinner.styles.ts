2353417d8099b8531e4f5ba27e2f5c730a8f3ddb
Trever Mock <trever.mock@prominentedge.com>
2020-10-09 08:52:34 -0600

    small changes to button with spinner styles

--- src/components/buttons/button-with-spinner.styles.ts
+++ src/components/buttons/button-with-spinner.styles.ts
@@ -2,11 +2,9 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 export default makeStyles((theme: Theme) => createStyles({
   root: {
-    display: 'flex',
-    alignItems: 'center',
+    margin: theme.spacing(0),
   },
   wrapper: {
-    margin: theme.spacing(1),
     position: 'relative',
   },
   buttonProgress: {
